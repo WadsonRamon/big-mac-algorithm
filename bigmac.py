ec8d66db581ec24843b40fc5dd04db77a0af27fa
WadsonRamon <wadsonramon1102@gmail.com>
2026-03-14 22:18:56 -0400

    add bigmac algorithm

---
+++ bigmac.py
@@ -0,0 +1,32 @@
+def dois_hamburgueres():
+    print("dois hamburgeres")
+
+def alface():
+    print("alface")
+
+def queijo():
+    print("queijo")
+
+def molho_especial():
+    print("molho especial")
+
+def cebola():
+    print("cebola")
+
+def picles():
+    print("picles")
+
+def pao_com_gergelim():
+    print("e um pão com gergelim")
+
+
+def big_mac():
+    dois_hamburgueres()
+    alface()
+    queijo()
+    molho_especial()
+    cebola()
+    picles()
+    pao_com_gergelim()
+
+big_mac()
